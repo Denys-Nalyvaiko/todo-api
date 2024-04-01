28e5cb5798b36a7c74a4204462abe729fd23c065
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-04-01 13:59:22 +0200

    fix: task base date format

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -20,7 +20,7 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
-    date = Column(DateTime)
+    date = Column(Date)
     is_completed = Column(Boolean, default=False)
     is_important = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'))
